9130d2e2f8dc7279bde609c2d1e50ecb18b5fce6
Marcus Poon <marcuspoon2@gmail.com>
2021-10-08 12:05:20 -0400

    added sign-up function

--- config/config/settings.py
+++ config/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
