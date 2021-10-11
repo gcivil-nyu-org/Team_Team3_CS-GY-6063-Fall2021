ad70d239cb05e3acadb788f318528dd78b2b389c
Indu Ramesh <induramesh@Indus-MBP.fios-router.home>
2021-10-11 14:11:56 -0400

    signup page takes an email and sends a confirmation email to user. Works in local host.  Also added gitignore that ignores committing pycache files, logs, and local db

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-
-from .views import SignUpView
-
-
+from django.conf.urls import url
+from . import views
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    #url(r'^$', views.home, name='home'),
+    url(r'^signup/$', views.signup, name='signup'),
+    # needed to update match repetitions
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,6}-[0-9A-Za-z]{1,32})/$',
+        views.activate, name='activate'),
 ]
