f5082840d1094dfd23ba03d5ffccc994ae23fea0
Marcus Poon <marcuspoon2@gmail.com>
2021-10-12 19:28:18 -0400

    added / to end of accounts/signup/ in config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
-    path('accounts/signup', TemplateView.as_view(template_name='signup.html'), name='signup'), # new
+    path('accounts/signup/', TemplateView.as_view(template_name='signup.html'), name='signup'), # new
 ]
