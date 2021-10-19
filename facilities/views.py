ee2f75fe40ee73c676ab467c6d0af841e5fe7a46
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-10-19 00:55:31 -0400

    add facilities data by id to details page

--- facilities/views.py
+++ facilities/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from maps.facilities_data import read_facilities_data
+import json
 
 def show(request, id):
-    return HttpResponse(f'this will be the page for the facility with mapbox objectid: { id }')
+    data = json.loads(read_facilities_data());
+    currentFacility = data[str(id)];
+
+    return HttpResponse(f'<div> this will be the page for the facility with mapbox objectid: { id } </div> <div>{currentFacility}</div>')
