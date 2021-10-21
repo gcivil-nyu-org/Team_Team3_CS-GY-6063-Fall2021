a289ee12f0f2e0c3d0cabbb3b743a49131a4f162
Indu Ramesh <induramesh@Indus-MBP.fios-router.home>
2021-10-21 17:13:05 -0400

    triggering travis

--- maps/facilities_data.py
+++ maps/facilities_data.py
@@ -4,16 +4,17 @@ import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 script_dir = os.path.dirname(BASE_DIR)
-file_path = os.path.join(BASE_DIR, 'maps/athletic_facilities.json')
+file_path = os.path.join(BASE_DIR, "maps/athletic_facilities.json")
+
 
 def read_facilities_data():
     with open(file_path) as file:
         data = json.load(file)
         dic = {}
         res = {}
-        for i in range(len(data['features'])):
-            currentObj = data['features'][i]
-            objId = currentObj['properties']['objectid'];
-            res[objId] = currentObj['properties'];
+        for i in range(len(data["features"])):
+            currentObj = data["features"][i]
+            objId = currentObj["properties"]["objectid"]
+            res[objId] = currentObj["properties"]
         dic = res
         return json.dumps(dic)
--- maps/views.py
+++ maps/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from config.settings import MAPBOX_TOKEN
 
+
 def default_map(request):
     mapbox_access_token = MAPBOX_TOKEN
-    return render(request, 'maps/map.html', 
-                  { 'mapbox_access_token': mapbox_access_token })
+    return render(
+        request, "maps/map.html", {"mapbox_access_token": mapbox_access_token}
+    )
