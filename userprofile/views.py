f6df755215f3ea8926fffb451f690202269f1dee
Aahbree <sp6180@nyu.edu>
2021-10-24 11:50:08 -0400

    User Profile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .models import Profile
 from .forms import ProfileUpdateForm
+
+
 # Create your views here.
 @login_required
 def userprofile(request):
