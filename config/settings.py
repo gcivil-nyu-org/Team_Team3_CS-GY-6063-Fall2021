9041b4bb5dadf9b7d6e2100baa96c5131a13cba3
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-10-25 20:54:16 -0400

    move django_heroku settings to bottom of file

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 if '/app' in os.environ['HOME']:
     import django_heroku
-    django_heroku.settings(locals())
     
 load_dotenv()  # loads the configs from .env
 
@@ -147,5 +146,5 @@ EMAIL_PORT = 587
 MAPBOX_TOKEN = str(os.getenv("MAPBOX_TOKEN"))
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-django_heroku.settings(locals(), test_runner=False)
+if '/app' in os.environ['HOME']:
+    django_heroku.settings(locals(), test_runner=False)
