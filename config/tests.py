19b7b2a1093d68dc3685fd4cabef36688a99605d
Indu Ramesh <induramesh@Indus-MBP.fios-router.home>
2021-10-31 23:55:25 -0400

    account dropdown/dashboard that opens on right side

--- config/tests.py
+++ config/tests.py
@@ -28,6 +28,20 @@ class TestUserLogin(TestCase):
         user = authenticate(username = 'not_a_valid_username_222', password = 'incorrectpassword')
         self.assertIsNone(user)
 
+    #Idea: Log Out is only in the dropdown. Tets that only a logged in user can see this string in the dropdown
+    def test_logged_in_user_sees_dashboard_dropdown(self):
+        c = Client()
+        status = c.login(username = 'test_login', password = 'secret_111')
+        response = c.get(reverse('login'))
+        self.assertContains(response, 'Log Out')
+
+    #Idea: Tets that a logged out user cannot see the dropdown, because response does not contain Log Out
+    def logged_out_user_can_log_in(self):
+        c = Client()
+        status = c.logout()
+        response = c.get(reverse('logout'))
+        self.assertNotContains(response, 'Log Out')
+
 class TestUserLoginViews(TestCase):
 
         def test_view_url_exists_in_expected_location(self):
@@ -44,4 +58,3 @@ class TestUserLoginViews(TestCase):
             response = c.get(reverse('login'))
             self.assertEqual(response.status_code, 200)
             self.assertTemplateUsed(response, 'registration/login.html')
-        
