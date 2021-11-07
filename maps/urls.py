2e49742bcfddddfffdf569ec33e67fc88fb67fab
Aahbree <sp6180@nyu.edu>
2021-11-07 16:44:25 -0500

    Separate map for every activity

--- maps/urls.py
+++ maps/urls.py
@@ -3,5 +3,21 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"", views.default_map, name="default_map"),
+    url("map", views.default_map, name="default_map"),
+    url("basketball", views.basketball_map, name="basketball_map"),
+    url("baseball", views.baseball_map, name="baseball_map"),
+    url("netball", views.netball_map, name="netball_map"),
+    url("softball", views.softball_map, name="softball_map"),
+    url("kickball", views.kickball_map, name="kickball_map"),
+    url("handball", views.handball_map, name="handball_map"),
+    url("bocce", views.bocce_map, name="bocce_map"),
+    url("frisbee", views.frisbee_map, name="frisbee_map"),
+    url("rugby", views.rugby_map, name="rugby_map"),
+    url("cricket", views.cricket_map, name="cricket_map"),
+    url("hockey", views.hockey_map, name="hockey_map"),
+    url("volleyball", views.volleyball_map, name="volleyball_map"),
+    url("flag_football", views.flag_football_map, name="flag_football_map"),
+    url("tennis", views.tennis_map, name="tennis_map"),
+    url("lacrosse", views.lacrosse_map, name="lacrosse_map"),
+    url("football", views.football_map, name="football_map"),
 ]
--- maps/views.py
+++ maps/views.py
@@ -7,3 +7,115 @@ def default_map(request):
     return render(
         request, "maps/map.html", {"mapbox_access_token": mapbox_access_token}
     )
+
+
+def basketball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/basketball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def baseball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/baseball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def handball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/handball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def kickball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/kickball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def bocce_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/bocce.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def frisbee_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/frisbee.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def flag_football_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/flag_football.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def rugby_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/rugby.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def cricket_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/cricket.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def tennis_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/tennis.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def hockey_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/hockey.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def lacrosse_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/lacrosse.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def netball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/netball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def softball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/softball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def volleyball_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/volleyball.html", {"mapbox_access_token": mapbox_access_token}
+    )
+
+
+def football_map(request):
+    mapbox_access_token = MAPBOX_TOKEN
+    return render(
+        request, "maps/football.html", {"mapbox_access_token": mapbox_access_token}
+    )
