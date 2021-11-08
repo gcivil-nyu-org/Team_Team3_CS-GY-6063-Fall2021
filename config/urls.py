20e12b1cd509881433b1fea868636b1f390e99f4
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-11-07 21:56:03 -0500

    add joined events to home page

--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,13 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from config.views import HomePageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("", HomePageView.as_view(template_name="home.html"), name="home"),  # new
     path(
         "accounts/signup/",
         TemplateView.as_view(template_name="signup.html"),
