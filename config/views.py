2968983a34b679cc0b013586d629fb2a1c9b574a
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-11-08 11:41:20 -0500

    fix: user event registration only queried if user is authenticated

--- config/views.py
+++ config/views.py
@@ -1,12 +1,13 @@
 from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
 
 from events.models import EventRegistration
-
+    
 class HomePageView(TemplateView):
-
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["events"] = EventRegistration.objects.filter(user = self.request.user)
+        if(self.request.user.is_authenticated):
+          context["events"] = EventRegistration.objects.filter(user = self.request.user)
         return context
