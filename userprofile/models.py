69463c19cb04f254d40371a33441ab10d64ea6b6
Aahbree <sp6180@nyu.edu>
2021-11-08 22:03:01 -0500

    Removing unnecessary fields

--- userprofile/models.py
+++ userprofile/models.py
@@ -33,29 +33,33 @@ class Profile(models.Model):
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
     bocce = models.BooleanField(default=False)
     frisbee = models.BooleanField(default=False)
-    tBall = models.BooleanField(default=False)
-    adultBaseball = models.BooleanField(default=False)
-    adultFootball = models.BooleanField(default=False)
-    adultSoftball = models.BooleanField(default=False)
-    basketball = models.BooleanField(default=False)
+    adultBaseball = models.BooleanField(default=False, verbose_name="Baseball")
+    adultFootball = models.BooleanField(default=False, verbose_name="Football")
+    adultSoftball = models.BooleanField(default=False, verbose_name="Softball")
+    basketball = models.BooleanField(default=False, verbose_name="Basketball")
     cricket = models.BooleanField(default=False)
     flagFootball = models.BooleanField(default=False)
     handball = models.BooleanField(default=False)
     hockey = models.BooleanField(default=False)
     kickball = models.BooleanField(default=False)
     lacrosse = models.BooleanField(default=False)
-    littleLeagueBaseball = models.BooleanField(default=False)
-    littleLeagueSoftball = models.BooleanField(default=False)
     netball = models.BooleanField(default=False)
     rugby = models.BooleanField(default=False)
     tennis = models.BooleanField(default=False)
     volleyball = models.BooleanField(default=False)
-    youthFootball = models.BooleanField(default=False)
     hiking = models.BooleanField(default=False)
     location = models.CharField(
-        max_length=20, choices=LOCATION_CHOICES, default="select"
+        verbose_name="Borough",
+        max_length=20,
+        choices=LOCATION_CHOICES,
+        default="select",
     )
-    distance = MultiSelectField(choices=BOROUGH_CHOICES, max_choices=5, blank=True)
+    distance = MultiSelectField(
+        verbose_name="Boroughs Willing to Travel:",
+        choices=BOROUGH_CHOICES,
+        max_choices=5,
+        blank=True,
+    )
     car = models.CharField(max_length=10, choices=CAR, default="select")
 
     def __str__(self):
