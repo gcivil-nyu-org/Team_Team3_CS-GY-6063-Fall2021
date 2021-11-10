150e28d302778f40bd34ca9b82cfcc890dbd530d
Marcus Poon <marcuspoon2@gmail.com>
2021-11-10 16:13:22 -0500

    delete logic

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url("edit_profile", views.profile, name="profile_edit"),
     url("profile", views.profile_page, name="profile_page"),
+    url("delete", views.delete_profile, name="profile_delete"),
 ]
