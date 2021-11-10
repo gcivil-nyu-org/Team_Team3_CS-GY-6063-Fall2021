1b6d6aa3867a56cba6fae51aadc73bced86ce1b8
Marcus Poon <marcuspoon2@gmail.com>
2021-11-10 16:18:41 -0500

    deleted extra imports

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import logout as auth_logout, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import UserDeleteProfileForm, UserUpdateForm, ProfileUpdateForm
