72eb0c22cfa63a6eb117231c0a5bb8d97bd5efc2
Marcus Poon <marcuspoon2@gmail.com>
2021-11-11 11:55:34 -0500

    fix travis errors

--- userprofile/tests/test_profile_deletion.py
+++ userprofile/tests/test_profile_deletion.py
@@ -1,10 +1,8 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
-from userprofile.forms import ProfileUpdateForm
 from userprofile.models import Profile
-from events.models import Event
-from userprofile.views import UserDeleteProfileForm, delete_profile
+from userprofile.views import delete_profile
 
 
 class TestUserProfileCreation(TestCase):
@@ -18,7 +16,7 @@ class TestUserProfileCreation(TestCase):
     def test_delete_existing_user(self):
         #check that the user and their profile exists
         user = User.objects.get(username="testuser")
-        profile = Profile.objects.get(user=user.pk)
+        Profile.objects.get(user=user.pk)
 
         response = self.c.post('userprofile/delete_profile')
         request = response.wsgi_request
@@ -27,7 +25,7 @@ class TestUserProfileCreation(TestCase):
         with self.assertRaises(User.DoesNotExist):
             user = User.objects.get(username="testuser")
         with self.assertRaises(Profile.DoesNotExist):   
-            profile = Profile.objects.get(user=user.pk)
+            Profile.objects.get(user=user.pk)
 
     def test_get_template_no_deletion(self):
 
@@ -51,3 +49,4 @@ class TestUserProfileCreation(TestCase):
         response = self.c.get(reverse("profile_delete"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "userprofile/delete_profile.html")
+        
