d84550eca9b8f30c37489d355d4c7f471a0fad0f
Aahbree <sp6180@nyu.edu>
2021-11-12 23:05:28 -0500

    Log In required for every event page

--- events/urls.py
+++ events/urls.py
@@ -1,12 +1,25 @@
 from django.urls import path
-from .views import EventsListView, EventsCreateView, EventDetailView, EventUpdateView, EventDeleteView, event_add_attendance, event_cancel_attendance
+from .views import (
+    EventsListView,
+    EventsCreateView,
+    EventDetailView,
+    EventUpdateView,
+    EventDeleteView,
+    event_add_attendance,
+    event_cancel_attendance,
+)
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-  path('', EventsListView.as_view(), name='events-list'),
-  path('<pk>/', EventDetailView.as_view(), name='event-detail'),
-  path("<pk>/join/", event_add_attendance, name="join-event"),
-  path("<pk>/unjoin/", event_cancel_attendance, name="unjoin-event"),
-  path('<pk>/update/', EventUpdateView.as_view(), name='event-update'),
-  path('<pk>/delete/', EventDeleteView.as_view(), name='event-delete'),
-  path("new/<id>/", EventsCreateView.as_view(), name="add-event"),
+    path("", login_required(EventsListView.as_view()), name="events-list"),
+    path("<pk>/", login_required(EventDetailView.as_view()), name="event-detail"),
+    path("<pk>/join/", event_add_attendance, name="join-event"),
+    path("<pk>/unjoin/", event_cancel_attendance, name="unjoin-event"),
+    path(
+        "<pk>/update/", login_required(EventUpdateView.as_view()), name="event-update"
+    ),
+    path(
+        "<pk>/delete/", login_required(EventDeleteView.as_view()), name="event-delete"
+    ),
+    path("new/<id>/", login_required(EventsCreateView.as_view()), name="add-event"),
 ]
