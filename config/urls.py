38fbaaadb926f1d3b392eb794f8c4c30ee75e739
Aahbree <sp6180@nyu.edu>
2021-11-13 20:07:21 -0500

    Password reset issue fixed

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,55 @@ from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from config.views import HomePageView
+from django.contrib.auth import views as auth_views
+from accounts.forms import EmailValidationOnForgotPassword
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "accounts/password_reset/",
+        auth_views.PasswordResetView.as_view(
+            form_class=EmailValidationOnForgotPassword
+        ),
+        name="password_reset",
+    ),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", HomePageView.as_view(template_name="home.html"), name="home"),  # new
+    path(
+        "accounts/signup/",
+        TemplateView.as_view(template_name="signup.html"),
+        name="signup",
+    ),  # new
+    path("maps/", include("maps.urls")),
+    path("facilities/", include("facilities.urls")),
+    path("events/", include("events.urls")),
+    path("userprofile/", include("userprofile.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""congif URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from config.views import HomePageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
