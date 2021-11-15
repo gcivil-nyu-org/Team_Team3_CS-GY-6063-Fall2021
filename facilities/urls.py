a745cef1ed13c3b05ed0ebb6b9bed9615433d924
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-11-15 01:46:07 -0500

    add seperate view to display all events at a location

--- facilities/urls.py
+++ facilities/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:sport>/<int:id>", views.show, name="show"),
+    path("<int:id>", views.show, name="show"),
+    path("<str:sport>/<int:id>", views.showWithSport, name="show-with-sport"),
     path("hiking/trails/<int:id>", views.trails, name="trails"),
 ]
--- facilities/views.py
+++ facilities/views.py
@@ -8,7 +8,7 @@ import json
 # import ast
 
 
-def show(request, sport, id):
+def show(request, id):
     data = json.loads(read_facilities_data())
     currentFacility = data[str(id)]
 
@@ -47,6 +47,59 @@ def show(request, sport, id):
     return render(
         request,
         "maps/facilities.html",
+        {
+            "sports": sports,
+            "id": id,
+            "currentFacility": currentFacility,
+            "name": name,
+            "accessible": accessible,
+            "dimensions": dimensions,
+            "wheelchair": wheelchair,
+            "events": eventsAtLocation,
+            "coordinates": coordinates,
+            "mapbox_access_token": MAPBOX_TOKEN
+        },
+    )
+
+def showWithSport(request, sport, id):
+    data = json.loads(read_facilities_data())
+    currentFacility = data[str(id)]
+
+    sports = {
+         'Bocce': currentFacility['bocce'],
+         'Track': currentFacility["track_and"],
+         'Frisbee': currentFacility["frisbee"],
+         'T Ball': currentFacility["t_ball"],
+         'Baseball': currentFacility["adult_base"],
+         'Football': currentFacility["adult_foot"],
+         'Softball': currentFacility["adult_soft"],
+         'Basketball': currentFacility["basketball"],
+         'Cricket': currentFacility["cricket"],
+         'Flag Football': currentFacility["flagfootba"],
+         'Handball': currentFacility["handball"],
+         'Hockey': currentFacility["hockey"],
+         'Kickball': currentFacility["kickball"],
+         'Lacrosse': currentFacility["lacrosse"],
+         # littleLeagueBaseballOne = currentFacility["ll_baseb_1"]
+         # littleLeagueBaseballTwo = currentFacility["ll_baseb_2"]
+         # littleLeagueSoftball = currentFacility["ll_softbal"]
+         'Netball': currentFacility["netball"],
+         'Rugby': currentFacility["rugby"],
+         'Tennis': currentFacility["tennis"],
+         'Volleyball': currentFacility["volleyball"]
+    }
+
+    name = currentFacility["name"]
+    accessible = currentFacility["accessible"]
+    # description = currentFacility["descriptio"]
+    dimensions = currentFacility["dimensions"]
+    wheelchair = currentFacility["wheelchair"]
+    coordinates = currentFacility['geometry']['coordinates']
+
+    eventsAtLocation = Event.objects.filter(locationId = id);
+    return render(
+        request,
+        "maps/facilitiesWithAdd.html",
         {
             "sport": sport,
             "sports": sports,
