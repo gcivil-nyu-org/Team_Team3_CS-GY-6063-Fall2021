18b87b67371bf953dfea92b97dc683724893a27c
Aahbree <sp6180@nyu.edu>
2021-11-16 13:17:49 -0500

    fixed 2 bugs: 1) Auto-populate date 2) Update event cannot allow past date

--- events/models.py
+++ events/models.py
@@ -4,54 +4,58 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 
-class Event(models.Model):
-  name = models.CharField(max_length=100);
-  description = models.TextField();
-  address = models.TextField();
-  locationId = models.CharField(max_length=100);
-  date = models.DateTimeField();
-  dateCreated = models.DateTimeField(default=timezone.now)
-  owner = models.ForeignKey(User, on_delete=models.CASCADE);
-  borough = models.CharField(max_length=20);
 
-  def clean(self, *args, **kwargs):
-    if self.date < timezone.now():
-      raise ValidationError("The date cannot be in the past!")
-      super(Event, self).clean(*args, **kwargs)
-  
-  class Meta:
+class Event(models.Model):
+    def no_past(value):
+        if value < timezone.now():
+            raise ValidationError("The date cannot be in the past!")
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    address = models.TextField()
+    locationId = models.CharField(max_length=100)
+    date = models.DateTimeField(verbose_name="Event Date", validators=[no_past])
+    dateCreated = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    borough = models.CharField(max_length=20)
+
+    class Meta:
         verbose_name = "event"
         verbose_name_plural = "events"
 
-  def get_registrations(self):
-    return EventRegistration.objects.filter(event = self)
+    def get_registrations(self):
+        return EventRegistration.objects.filter(event=self)
 
-  def add_user_to_list_of_attendees(self, user):
-    registration = EventRegistration.objects.create(user = user,  event = self, time_registered = timezone.now())
-    return registration
+    def add_user_to_list_of_attendees(self, user):
+        registration = EventRegistration.objects.create(
+            user=user, event=self, time_registered=timezone.now()
+        )
+        return registration
 
-  def remove_user_from_list_of_attendees(self, user):
-      registration = EventRegistration.objects.get(user = user, event = self)
-      registration.delete()
-      return True
+    def remove_user_from_list_of_attendees(self, user):
+        registration = EventRegistration.objects.get(user=user, event=self)
+        registration.delete()
+        return True
 
-  def get_absolute_url(self):
-    return reverse("event-detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("event-detail", kwargs={"pk": self.pk})
+
 
 class EventRegistration(models.Model):
-    event = models.ForeignKey(Event,verbose_name='Event', on_delete=models.CASCADE)
-    user = models.ForeignKey(User,verbose_name='Attendee', on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, verbose_name="Event", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="Attendee", on_delete=models.CASCADE)
     time_registered = models.DateTimeField()
+
     def __str__(self):
-      return self.user.username
+        return self.user.username
 
     class Meta:
-      verbose_name = 'Attendee for event'
-      verbose_name_plural = 'Attendees for events'
-      ordering = ['time_registered']
-      unique_together = ('event', 'user')
+        verbose_name = "Attendee for event"
+        verbose_name_plural = "Attendees for events"
+        ordering = ["time_registered"]
+        unique_together = ("event", "user")
 
     def save(self, *args, **kwargs):
-      if self.id is None and self.time_registered is None:
-        self.time_registered = timezone.now()
-      super(EventRegistration, self).save(*args, **kwargs)
+        if self.id is None and self.time_registered is None:
+            self.time_registered = timezone.now()
+        super(EventRegistration, self).save(*args, **kwargs)
--- events/views.py
+++ events/views.py
@@ -1,110 +1,118 @@
 from django.contrib.auth.decorators import login_required
 from .models import Event, EventRegistration
-from django.shortcuts import redirect 
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
-  CreateView,
-  ListView, 
-  DetailView,
-  UpdateView,
-  DeleteView
-  )
+    CreateView,
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 from django import forms
 from maps.facilities_data import read_facilities_data
 import json
 
 
 class EventsListView(ListView):
-  model = Event
-  template_name = 'events/events_list.html'
-  context_object_name = 'events'
-  ordering=['-dateCreated']
+    model = Event
+    template_name = "events/events_list.html"
+    context_object_name = "events"
+    ordering = ["-dateCreated"]
+
 
 class EventDetailView(DetailView):
-  model = Event
-  template_name = 'events/events_detail.html'
+    model = Event
+    template_name = "events/events_detail.html"
 
-  def get_context_data(self, **kwargs):
-    isAttending = False
-    context = super(EventDetailView, self).get_context_data(**kwargs)
-    context['attendees'] = EventRegistration.objects.filter(event = context["object"]);
-    for attendee in context['attendees']:
-      if attendee.user == self.request.user:
-        isAttending = True
-    context['isAttending'] = isAttending
-    return context
+    def get_context_data(self, **kwargs):
+        isAttending = False
+        context = super(EventDetailView, self).get_context_data(**kwargs)
+        context["attendees"] = EventRegistration.objects.filter(event=context["object"])
+        for attendee in context["attendees"]:
+            if attendee.user == self.request.user:
+                isAttending = True
+        context["isAttending"] = isAttending
+        return context
+
 
 @login_required
 def event_add_attendance(request, pk):
-  this_event = Event.objects.get(pk=pk)
-  this_event.add_user_to_list_of_attendees(user=request.user)
-  return redirect("event-detail", pk)
+    this_event = Event.objects.get(pk=pk)
+    this_event.add_user_to_list_of_attendees(user=request.user)
+    return redirect("event-detail", pk)
+
 
 @login_required
 def event_cancel_attendance(request, pk):
-  this_event = Event.objects.get(pk=pk)
-  this_event.remove_user_from_list_of_attendees(request.user)
-  return redirect("event-detail", pk)
+    this_event = Event.objects.get(pk=pk)
+    this_event.remove_user_from_list_of_attendees(request.user)
+    return redirect("event-detail", pk)
+
 
 class DateInput(forms.DateTimeInput):
-  input_type='datetime-local'
+    input_type = "datetime-local"
+
 
 class CreateEventForm(forms.ModelForm):
-  class Meta:
-    model = Event
-    fields = ['name', 'description', 'address', 'date']
-    widgets = {'date' : DateInput()}
+    class Meta:
+        model = Event
+        fields = ["name", "description", "address", "date"]
+        widgets = {"date": DateInput()}
+
 
 class UpdateEventForm(forms.ModelForm):
-  class Meta:
-    model = Event
-    fields = ['name', 'description', 'address', 'locationId', 'date']
-    widgets = {'date' : DateInput()}
+    class Meta:
+        model = Event
+        fields = ["name", "description", "address", "locationId", "date"]
+        date = forms.DateField(widget=DateInput(), initial=DateInput())
+
 
 class EventsCreateView(LoginRequiredMixin, CreateView):
-  form_class = CreateEventForm
-  model = Event
+    form_class = CreateEventForm
+    model = Event
 
-  def form_valid(self, form):
-    form.instance.owner = self.request.user
-    form.instance.locationId = self.kwargs.get('id', None)
-    data = json.loads(read_facilities_data())
-    currentFacility = data[str(self.kwargs.get('id', None))]
-    borough = currentFacility['borough']
-    if borough == 'B':
-      form.instance.borough = 'Brooklyn'
-    elif borough == 'M':
-      form.instance.borough = 'Manhattan'
-    elif borough == 'X':
-      form.instance.borough = 'Bronx'
-    elif borough == 'R':
-      form.instance.borough = 'Staten Island'
-    else:
-      form.instance.borough = 'Queens'
-    return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        form.instance.locationId = self.kwargs.get("id", None)
+        data = json.loads(read_facilities_data())
+        currentFacility = data[str(self.kwargs.get("id", None))]
+        borough = currentFacility["borough"]
+        if borough == "B":
+            form.instance.borough = "Brooklyn"
+        elif borough == "M":
+            form.instance.borough = "Manhattan"
+        elif borough == "X":
+            form.instance.borough = "Bronx"
+        elif borough == "R":
+            form.instance.borough = "Staten Island"
+        else:
+            form.instance.borough = "Queens"
+        return super().form_valid(form)
+
 
 class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-  form_class = UpdateEventForm
-  model = Event
+    form_class = UpdateEventForm
+    model = Event
 
-  def form_valid(self, form):
-    form.instance.owner = self.request.user
-    return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
-  def test_func(self):
-    event = self.get_object()
-    if self.request.user == event.owner:
-      return True
-    return False
+    def test_func(self):
+        event = self.get_object()
+        if self.request.user == event.owner:
+            return True
+        return False
+
 
 class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-  model = Event
-  template_name = 'events/event_confirm_delete.html'
-  success_url = '/'
+    model = Event
+    template_name = "events/event_confirm_delete.html"
+    success_url = "/"
 
-  def test_func(self):
-    event = self.get_object()
-    if self.request.user == event.owner:
-      return True
-    return False
-
+    def test_func(self):
+        event = self.get_object()
+        if self.request.user == event.owner:
+            return True
+        return False
