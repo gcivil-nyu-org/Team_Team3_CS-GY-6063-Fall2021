914932832915cb556b9d5598c100c4f39e4f3739
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-11-16 20:56:03 -0500

    comment out unused import

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from multiselectfield import MultiSelectField
-from django.core.files.storage import default_storage as storage
+# from django.core.files.storage import default_storage as storage
 
 
 LOCATION_CHOICES = (
