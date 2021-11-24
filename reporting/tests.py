75c1a7744c31d3bb2b0e7d3e3edf1ce0d49bc544
Aahbree <sp6180@nyu.edu>
2021-11-23 21:17:31 -0500

    fix/travis

--- reporting/tests.py
+++ reporting/tests.py
@@ -1,6 +1,5 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class TestReportingViews(TestCase):
