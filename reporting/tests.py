abbcf8ffba1617b729f501ee97bb676e98b96033
Indu Ramesh <induramesh@Indus-MBP.fios-router.home>
2021-11-29 17:35:37 -0500

    testing report misbehavior

--- reporting/tests.py
+++ reporting/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from reporting.forms import ReportNoShowForm,ReportMisbehaviorForm
-from .models import Noshow
+from .models import Noshow, Misbehavior
 
 
 class TestReportingViews(TestCase):
@@ -20,3 +20,10 @@ class TestReportingViews(TestCase):
         self.user2 = User.objects.create(username = 'test_login_2')
         c.post('/reporting/report_noshow', {'name': self.user2.id, 'description': 'description'})
         self.assertEqual(Noshow.objects.filter(name=self.user2).exists(), True)
+
+    def test_reporting_misbehavior_success(self):
+        c = Client()
+        c.login(username = 'test_login', password = 'secret_111')
+        self.user2 = User.objects.create(username = 'test_login_2')
+        c.post('/reporting/report_misbehavior', {'name': self.user2.id, 'description': 'description'})
+        self.assertEqual(Misbehavior.objects.filter(name=self.user2).exists(), True)
