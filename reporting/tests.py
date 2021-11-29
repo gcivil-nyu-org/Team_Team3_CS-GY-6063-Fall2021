15db45cbf472cb9f1526c781dcc7ddf51ca37112
Indu Ramesh <induramesh@Indus-MBP.fios-router.home>
2021-11-29 17:34:06 -0500

    testing no show

--- reporting/tests.py
+++ reporting/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from reporting.forms import ReportNoShowForm,ReportMisbehaviorForm
+from .models import Noshow
 
 
 class TestReportingViews(TestCase):
@@ -11,4 +13,10 @@ class TestReportingViews(TestCase):
     def test_ContactUs_view(self):
         response = self.client.get('reporting/contactus.html')
         self.assertEqual(response.status_code, 404)
-       
+
+    def test_reporting_no_show_success(self):
+        c = Client()
+        c.login(username = 'test_login', password = 'secret_111')
+        self.user2 = User.objects.create(username = 'test_login_2')
+        c.post('/reporting/report_noshow', {'name': self.user2.id, 'description': 'description'})
+        self.assertEqual(Noshow.objects.filter(name=self.user2).exists(), True)
