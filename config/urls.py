5323ba8fb2ddbc3322a78b00873a76926f8b9537
Aahbree <sp6180@nyu.edu>
2021-12-07 17:53:18 -0500

    Redirecting favicon calls

--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,17 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from config.views import HomePageView
 from accounts.forms import EmailValidationOnForgotPassword
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
     path(
         "accounts/password_reset/",
         auth_views.PasswordResetView.as_view(
@@ -46,7 +51,6 @@ urlpatterns = [
     path("", include("squad.urls")),
     path("messaging/", include("messaging.urls")),
     path("reporting/", include("reporting.urls")),
-
 ]
 
 if settings.DEBUG:
