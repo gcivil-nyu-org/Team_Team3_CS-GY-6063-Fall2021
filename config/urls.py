743213acb5edc64926e5571d01033269ad8a6838
Aahbree <sp6180@nyu.edu>
2021-12-08 09:00:28 -0500

    fix url.py

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 # config/urls.py
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -39,7 +40,11 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", HomePageView.as_view(template_name="home.html"), name="home"),  # new
-    path("about", TemplateView.as_view(template_name="about.html"), name="about"),
+    path(
+        "about",
+        login_required(TemplateView.as_view(template_name="about.html")),
+        name="about",
+    ),
     path(
         "accounts/signup/",
         TemplateView.as_view(template_name="signup.html"),
